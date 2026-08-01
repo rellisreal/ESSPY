55a2c441eba96df8982484a4834e708dda2d924b
rellisreal <gendarkdragon@gmail.com>
2026-08-01 15:27:47 +0800

    Adding some sample changes to assist with configuring a baseline allowing us to implement the usecase with ease

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api.model.base import Item
 
 app = FastAPI()
 
@@ -6,3 +7,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
+
+
+@app.get("/items/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
